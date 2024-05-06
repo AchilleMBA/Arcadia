3c25ffcd671826e02d60e29c7ce38b10ea4421a0
Achille Maboa <achille.maboa@icloud.com>
2024-05-06 09:55:22 +0200

    installation scss

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
