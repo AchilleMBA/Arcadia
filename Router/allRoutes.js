c534d7e1a3806584743402e30f98dfe181ccf740
Achille Maboa <achille.maboa@icloud.com>
2024-05-13 16:06:41 +0200

    mise a jour

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,16 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Connexion", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Connexion", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/services", "Service", "/pages/services.html"),
+    new Route("/habitats", "habitats", "/pages/habitats.html"),
+    new Route("/contact", "contact", "/pages/contact.html"),
+    new Route("/marais", "marais", "/pages/marais.html"),
+    new Route("/savane", "savane", "/pages/savane.html"),
+    new Route("/jungle", "jungle", "/pages/jungle.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
